12e2d5f238b35485aac29b835bc96e0661f12447
Chris Silich <chris.silich@gmail.com>
2019-02-20 13:19:41 -0500

    ready for students

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,6 +1,5 @@
-'use strict';
 
-var $video = document.querySelector('video'); // ideally, this would be more specific. What if there was more than one video?
+let $video = document.querySelector('video'); // ideally, this would be more specific. What if there was more than one video?
 
 document.querySelector('.play-pause').addEventListener('click', function () {
 	$video.play();
--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,6 @@
 
 let $video = document.querySelector('video'); // ideally, this would be more specific. What if there was more than one video?
 
-document.querySelector('.play-pause').addEventListener('click', () => {
+document.querySelector('.play-pause').addEventListener('click', function() {
 	$video.play()
 })
