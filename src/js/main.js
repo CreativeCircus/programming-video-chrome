dfdf003252abc40e007f3b54b87a843a7f059b8d
Chris Silich <chris.silich@gmail.com>
2019-02-27 12:48:29 -0500

    ready for students

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,5 @@
 
-let $video = document.querySelector('video'); // ideally, this would be more specific. What if there was more than one video?
+let $video = document.querySelector('video'); // ideally, this would be more specific. What if there was more than one video tag on the page?
 
 document.querySelector('.play-pause').addEventListener('click', function() {
 	$video.play()
