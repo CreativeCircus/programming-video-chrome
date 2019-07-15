d687bfa1599a9a40e02b21c58423399f8f6667de
Chris Silich <chris.silich@gmail.com>
2019-07-15 13:25:23 -0400

    teacher versions done

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,7 +1,87 @@
+"use strict";
 
-let $video = document.querySelector('video'); // ideally, this would be more specific. What if there was more than one video tag on the page?
+var $video = document.querySelector('video'); // ideally, this would be more specific. What if there was more than one video?
 
-document.querySelector('.play-pause').addEventListener('click', function () {
-	$video.play();
+var $playPause = document.querySelector('.play-pause');
+var $muteUnmute = document.querySelector('.mute-unmute');
+var $time = document.querySelector('.time');
+var $progressBar = document.querySelector('.progress-bar');
+var $fullScreen = document.querySelector('.full-screen');
+var $back15 = document.querySelector('.back-15');
+var $forward15 = document.querySelector('.forward-15');
+
+if (!$video.requestFullscreen && !$video.webkitRequestFullscreen) {
+  $fullScreen.style.display = "none";
+}
+
+$back15.addEventListener("click", function () {
+  $video.currentTime -= 15;
 });
+$forward15.addEventListener("click", function () {
+  $video.currentTime += 15;
+});
+$fullScreen.addEventListener("click", function () {
+  if ($video.requestFullscreen) {
+    $video.requestFullscreen();
+  } else if ($video.webkitRequestFullscreen) {
+    $video.webkitRequestFullscreen();
+  } else if ($video.mozRequestFullscreen) {
+    $video.mozRequestFullscreen();
+  } else if ($video.oRequestFullscreen) {
+    $video.oRequestFullscreen();
+  }
+});
+$playPause.addEventListener('click', function () {
+  if ($video.paused) {
+    $video.play();
+    $playPause.classList.add("active");
+  } else {
+    // it must be playing already
+    $video.pause();
+    $playPause.classList.remove("active");
+  }
+});
+$muteUnmute.addEventListener('click', function () {
+  if ($video.muted) {
+    $video.muted = false;
+    $muteUnmute.classList.remove("active");
+  } else {
+    $video.muted = true;
+    $muteUnmute.classList.add("active");
+  }
+}); // https://developer.mozilla.org/en-US/docs/Web/Guide/Events/Media_events
+
+$video.addEventListener("timeupdate", function () {
+  console.log("timeupdate", $video.currentTime); // https://developer.mozilla.org/en-US/docs/Web/API/HTMLMediaElement
+
+  $time.innerHTML = formatTime($video.currentTime) + " of " + formatTime($video.duration);
+  $progressBar.style.width = $video.currentTime / $video.duration * 100 + "%";
+});
+
+var formatTime = function formatTime(s) {
+  var m = 0;
+  var h = 0;
+  s = Math.floor(s);
+
+  while (s >= 60) {
+    s -= 60;
+    m++;
+  }
+
+  while (m >= 60) {
+    m -= 60;
+    h++;
+  }
+
+  s = String(s);
+  s = s.padStart(2, "0");
+
+  if (h) {
+    m = String(m);
+    m = m.padStart(2, "0");
+    return h + ":" + m + ":" + s;
+  } else {
+    return m + ":" + s;
+  }
+};
 //# sourceMappingURL=main.js.map
--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,96 @@
 
-let $video = document.querySelector('video'); // ideally, this would be more specific. What if there was more than one video tag on the page?
+let $video = document.querySelector('video'); // ideally, this would be more specific. What if there was more than one video?
+let $playPause = document.querySelector('.play-pause');
+let $muteUnmute = document.querySelector('.mute-unmute');
+let $time = document.querySelector('.time');
+let $progressBar = document.querySelector('.progress-bar');
+let $fullScreen = document.querySelector('.full-screen');
+let $back15 = document.querySelector('.back-15');
+let $forward15 = document.querySelector('.forward-15');
 
-document.querySelector('.play-pause').addEventListener('click', function() {
-	$video.play()
+if (!$video.requestFullscreen && !$video.webkitRequestFullscreen ) {
+	$fullScreen.style.display = "none";
+}
+$back15.addEventListener("click", () => {
+	$video.currentTime -= 15;
+});
+$forward15.addEventListener("click", () => {
+	$video.currentTime += 15;	
+});
+
+$fullScreen.addEventListener("click", () => {
+	if ($video.requestFullscreen) {
+		$video.requestFullscreen()
+	} else if ($video.webkitRequestFullscreen) {
+		$video.webkitRequestFullscreen()
+	} else if ($video.mozRequestFullscreen) {
+		$video.mozRequestFullscreen()
+	} else if ($video.oRequestFullscreen) {
+		$video.oRequestFullscreen()
+	}
 })
+
+$playPause.addEventListener('click', () => {
+	if ($video.paused) {
+		$video.play();
+		$playPause.classList.add("active");
+	} else { // it must be playing already
+		$video.pause()
+		$playPause.classList.remove("active");
+	}
+})
+
+
+$muteUnmute.addEventListener('click', () => {
+	if ($video.muted) {
+		$video.muted = false;
+		$muteUnmute.classList.remove("active");
+	} else {
+		$video.muted = true;
+		$muteUnmute.classList.add("active");
+	}
+})
+
+// https://developer.mozilla.org/en-US/docs/Web/Guide/Events/Media_events
+$video.addEventListener("timeupdate", () => {
+	console.log("timeupdate", $video.currentTime)
+
+	// https://developer.mozilla.org/en-US/docs/Web/API/HTMLMediaElement
+	$time.innerHTML = formatTime($video.currentTime) + " of " + formatTime($video.duration);
+
+	$progressBar.style.width = ($video.currentTime / $video.duration) * 100 + "%"; 
+
+})
+
+
+
+
+
+
+
+let formatTime = function(s) {
+	let m = 0;
+	let h = 0;
+	s = Math.floor(s);
+
+	while (s >= 60) {
+		s -= 60;
+		m++;
+	}
+	while (m >= 60) {
+		m -= 60;
+		h++;
+	}
+
+	s = String(s);
+	s = s.padStart(2, "0")
+
+	if (h) {
+		m = String(m);
+		m = m.padStart(2, "0")
+		return h + ":" + m  + ":" + s; 
+	} else {
+		return m  + ":" + s;
+	}
+}
+
