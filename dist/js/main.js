4eff1d6c06439d1d1a2b1411a68b9dd8daecb46e
Chris Silich <chris.silich@gmail.com>
2020-01-27 13:08:31 -0500

    js playlist thumb URLs

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,7 +1,25 @@
+"use strict";
 
-let $video = document.querySelector('video'); // ideally, this would be more specific. What if there was more than one video tag on the page?
+var videosData = [{
+  name: 'City Arial fly over',
+  video: 'dist/video/city.mp4',
+  thumb: 'dist/img/city.png'
+}, {
+  name: 'Earth from ISS',
+  video: 'dist/video/earth.mp4',
+  thumb: 'dist/img/earth.png'
+}, {
+  name: 'Rain',
+  video: 'dist/video/rain.mp4',
+  thumb: 'dist/img/rain.png'
+}, {
+  name: 'VU Meters',
+  video: 'dist/video/vu.mp4',
+  thumb: 'dist/img/vu.png'
+}];
+var videoElement = document.querySelector('video'); // ideally, this would be more specific. What if there was more than one video tag on the page?
 
 document.querySelector('.play-pause').addEventListener('click', function () {
-	$video.play();
+  videoElement.play();
 });
 //# sourceMappingURL=main.js.map
--- src/js/main.js
+++ src/js/main.js
@@ -1,22 +1,22 @@
 let videosData = [{
 		name: 'City Arial fly over',
 		video: 'dist/video/city.mp4',
-		thumb: 'dist/img/city.mp4',
+		thumb: 'dist/img/city.png',
 	},
 	{
 		name: 'Earth from ISS',
 		video: 'dist/video/earth.mp4',
-		thumb: 'dist/img/earth.mp4',
+		thumb: 'dist/img/earth.png',
 	},
 	{
 		name: 'Rain',
 		video: 'dist/video/rain.mp4',
-		thumb: 'dist/img/rain.mp4',
+		thumb: 'dist/img/rain.png',
 	},
 	{
 		name: 'VU Meters',
 		video: 'dist/video/vu.mp4',
-		thumb: 'dist/img/vu.mp4',
+		thumb: 'dist/img/vu.png',
 	},
 ]
 
