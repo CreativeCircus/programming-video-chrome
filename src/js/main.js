5acf423126022cb463aec02830b389c968c5d658
Chris Silich <chris.silich@gmail.com>
2020-01-27 12:49:17 -0500

    updates!

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,28 @@
+let videosData = [{
+		name: 'City Arial fly over',
+		video: 'dist/video/city.mp4',
+		thumb: 'dist/img/city.mp4',
+	},
+	{
+		name: 'Earth from ISS',
+		video: 'dist/video/earth.mp4',
+		thumb: 'dist/img/earth.mp4',
+	},
+	{
+		name: 'Rain',
+		video: 'dist/video/rain.mp4',
+		thumb: 'dist/img/rain.mp4',
+	},
+	{
+		name: 'VU Meters',
+		video: 'dist/video/vu.mp4',
+		thumb: 'dist/img/vu.mp4',
+	},
+]
 
-let $video = document.querySelector('video'); // ideally, this would be more specific. What if there was more than one video tag on the page?
 
-document.querySelector('.play-pause').addEventListener('click', function() {
-	$video.play()
+let videoElement = document.querySelector('video') // ideally, this would be more specific. What if there was more than one video tag on the page?
+
+document.querySelector('.play-pause').addEventListener('click', function () {
+	videoElement.play()
 })
